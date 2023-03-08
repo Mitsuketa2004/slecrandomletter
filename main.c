a1f8677e7d5521ce93c16e965208bc5a44a36614
Mitsuketa2004 <gabrielandrade1925@gmail.com>
2023-03-08 12:02:42 -0300

    Changes with pointers and logic of the algorithm but, still not working as expected

--- main.c
+++ main.c
@@ -2,34 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+char mainstr[] = "HelloWorld";
+int mainStrsze = sizeof(mainstr);	
 
-int createRandomL(){
-	int randnum = rand() % 63;
-	return randnum;
+void createRandomL(int *letter){
+	int randnum = (rand()%(254 - 33 + 1))+33;
+	*letter = randnum;
 }
-int checkStr(char* str1, char* str2){
-	int i;
-	for (i = 0; i <= sizeof(str1); i++){
+
+int checkStr(char *str1,char *str2){
+	for (int i = 0; i <= mainStrsze; i++){
 		if (str1[i] != str2[i]){
 			return 1;
 		}
 	}
 	return 0;
 }
+
 int main(){
 	srand(time(NULL));
-	char *strToF = "Hello World", *strFin;
-	
-	while((checkStr(strToF, strFin))){
-		for (int i = 0; i <= sizeof(strToF); i++){
-			int ibuf;
-			strFin[i] = createRandomL();
-			if (strFin[i] != strToF[i]){
+	char strbuf[mainStrsze];
+	strbuf[0] = "H";
+	while((checkStr(mainstr, strbuf))){
+		for (int i = 0; i <= mainStrsze; i++){
+			int ibuf = 0;
+			if (strbuf[i] != mainstr[i]){
 				i = ibuf;
-			}
+				}
+			int letBuffer;
+			createRandomL(&letBuffer);
+			printf("\n %s \n %i - %c", strbuf, i, letBuffer);
+			strbuf[i] = letBuffer;
 		}
+	
 	}
-	printf ("%s -%s", strToF, strFin);
-
+	printf("\n %i \n", mainStrsze);
+	printf ("%s - %s", mainstr, strbuf);
 return 0;
 }
