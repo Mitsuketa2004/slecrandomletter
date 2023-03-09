1bb2a95504771195b88e0eaf7e0d9beda9964ae8
Mitsuketa2004 <gabrielandrade1925@gmail.com>
2023-03-09 12:33:30 -0300

    att, the checker between the two strings is working, but not the randomizer of the letters. backup commit

--- main.c
+++ main.c
@@ -2,41 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-char mainstr[] = "HelloWorld";
+char mainstr[] = "CAGUEI";
 int mainStrsze = sizeof(mainstr);	
 
-void createRandomL(int *letter){
+void createRandomL(char *letter){
 	int randnum = (rand()%(254 - 33 + 1))+33;
 	*letter = randnum;
 }
 
-int checkStr(char *str1,char *str2){
-	for (int i = 0; i <= mainStrsze; i++){
-		if (str1[i] != str2[i]){
+int checkStr(char *str1,char  *str2){
+	for (int i = 0; i < mainStrsze; i++){
+		if (str1[i] != str2[i] ){			
 			return 1;
 		}
 	}
 	return 0;
 }
-
 int main(){
 	srand(time(NULL));
-	char strbuf[mainStrsze];
-	strbuf[0] = "H";
-	while((checkStr(mainstr, strbuf))){
-		for (int i = 0; i <= mainStrsze; i++){
-			int ibuf = 0;
-			if (strbuf[i] != mainstr[i]){
-				i = ibuf;
+	char *strbuf = malloc((mainStrsze) * sizeof(char));
+	while(checkStr(mainstr, strbuf) == 1){
+		int i = 0;
+			while(strbuf[i] < mainStrsze){
+				if (strbuf[i] == mainstr[i]){
+					i++;
+				}else{
+					createRandomL(&strbuf[i]);
 				}
-			int letBuffer;
-			createRandomL(&letBuffer);
-			printf("\n %s \n %i - %c", strbuf, i, letBuffer);
-			strbuf[i] = letBuffer;
-		}
-	
-	}
-	printf("\n %i \n", mainStrsze);
-	printf ("%s - %s", mainstr, strbuf);
+				
+			}
+		/**for (int i = 0; i < mainStrsze; i++){
+			if (i != mainStrsze && strbuf[i] != '\0'){
+				if(strbuf[i] != mainstr[i]){
+					createRandomL(&strbuf[i]);
+				} else {
+					i++;
+				}
+			}
+		}**/
+		printf (" %s - %s \n", mainstr, strbuf);
+	}	
 return 0;
 }
